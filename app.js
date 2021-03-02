db9a7b967155a450985a62a4e9c42418bed271af
RafrafiAhmed <ahmed.rafrafi25@gmail.com>
2021-03-02 17:28:25 +0100

    done

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const bodyParser= require('body-parser');
+const app=express();
+app.use(bodyParser.json());
+const dotenv = require('dotenv');
+
+
+
+const connectDB = require('./config/db');
+//load config
+dotenv.config({ path: './config/config.env'})
+connectDB();
+
+//Routes
+
+app.use('/', require('./routes/index'))
+app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,75 @@
+const express=require('express');
+const router = express.Router();
+
+
+const{ Employee } = require('../models/employee');
+
+//get all employees
+router.get('/api/employees', (req, res)=>{
+    Employee.find({}, (err, data)=>{
+        if(!err){
+            res.send(data);
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+//save employee
+router.post('/api/employee/add', (req, res)=>{
+    const emp=new Employee({
+        name:req.body.name,
+        email:req.body.email,
+        salary:req.body.salary
+    });
+    emp.save((err, data)=>{
+        res.status(200).json({code:200, message:"Employee Added Successfully",
+    addEmployee:data})
+    })
+})
+
+
+//Get single employee
+
+
+router.get('/api/employee/:id', (req, res)=>{
+    Employee.findById(req.params.id, (err, data)=>{
+        if(!err){
+            res.send(data)
+        }else{
+            console.log(err)
+        }
+    })
+})
+
+//update employee
+router.put('/api/employee/edit/:id', (req, res)=>{
+   const emp={
+       name:req.body.name,
+       email:req.body.email,
+       salary:req.body.salary
+   } 
+   Employee.findByIdAndUpdate(req.params.id ,{$set:emp}, {new:true} , 
+   (err,data)=>{
+       if(!err){
+           res.status(200).json({code:200, message:'Employee Update Successfully',
+        updateEmployee:data})
+       }else{
+           console.log(err)
+       }
+   } )
+})
+
+
+//delete employee
+router.delete('/api/employee/:id', (req, res)=>{
+    Employee.findByIdAndRemove(req.params.id, (err, data)=>{
+        if(!err){
+            res.status(200).json({code:200, message:"Employee Deleted Successfully",
+        deleteEmployee:data})
+        
+        }
+    })
+})
+
+module.exports=router;
